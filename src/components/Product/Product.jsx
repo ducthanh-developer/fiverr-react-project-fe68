572dec282f3f76ad54e31e50f3a682d41fa52aef
nguyenmya <80749352+nguyenmya@users.noreply.github.com>
2021-10-07 21:16:34 +0700

    mya push

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,97 @@
+
+import React from 'react'
+import Slider from "react-slick";
+import "../Product/Product.scss"
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "white" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default function Product() {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+  return (
+    <div className=" container Product__content">
+      <h2>Center Mode</h2>
+      <Slider {...settings}>
+        <div className="">
+          <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png" alt="..." />
+        </div>
+        <div>
+          <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png" alt="..." />
+        </div>
+        <div>
+          <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png" alt="..." />
+        </div>
+        <div>
+          <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png" alt="..." />
+        </div>
+        <div>
+          <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png" alt="..." />
+        </div>
+        <div>
+          <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png" alt="..." />
+
+        </div>
+        <div>
+            <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png" alt="" />
+          </div>
+          <div>
+          <img src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_255,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741674/translation-2x.png" alt="" />
+
+          </div>
+      </Slider>
+    </div>
+  )
+}
--- src/containers/client/Home/Home.jsx
+++ src/containers/client/Home/Home.jsx
@@ -1,12 +1,16 @@
+import Product from 'components/Product/Product';
+import Slider from 'components/Slider/Slider';
 import React, { Component, Fragment } from 'react';
 import MovieList from './MovieList/MovieList';
 export default class Home extends Component {
   render() {
     return (
       <>
+      <Slider/>
         {/* Banner */}
         {/* Filter */}
-        <MovieList />
+        {/* <MovieList /> */}
+        <Product/>
       </>
     );
   }
